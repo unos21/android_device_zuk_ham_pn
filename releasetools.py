7f7c81ec4d8fa2a042e2529db498d38ea6c43bcb
MartinJesus23 <xdddd23@gmail.com>
2017-04-27 20:09:51 +0200

    Remove TrustZone finally.

--- releasetools.py
+++ releasetools.py
@@ -107,13 +107,3 @@ def FullOTA_InstallEnd(info):
 
 def IncrementalOTA_InstallEnd(info):
   InstallRadioFiles(info)
-
-def AddTrustZoneAssertion(info, input_zip):
-  android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-trustzone\s*=\s*(\S+)', android_info)
-  if m:
-    versions = m.group(1).split('|')
-    if len(versions) and '*' not in versions:
-      cmd = 'assert(ham.verify_trustzone(' + ','.join(['"%s"' % tz for tz in versions]) + ') == "1");'
-      info.script.AppendExtra(cmd)
-  return
