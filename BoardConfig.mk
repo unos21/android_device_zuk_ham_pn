f2b427c1bb51fe20a13debb9420021c7521d6616
MartinJesus23 <xdddd23@gmail.com>
2017-04-27 19:50:31 +0200

    Remove Assert TrustZone version (1)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,9 +20,6 @@ DEVICE_PATH := device/zuk/ham
 # TODO: shouldn't be here
 BOARD_VENDOR := zuk
 
-# Assertions
-TARGET_BOARD_INFO_FILE ?= $(DEVICE_PATH)/board-info.txt
-
 #Include path
 TARGET_SPECIFIC_HEADER_PATH += $(DEVICE_PATH)/include
 
